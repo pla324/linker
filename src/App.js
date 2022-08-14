5559d616b8033632de9018551b9c0c35f589c31a
Ryan Barouki <ryanbarouki@gmail.com>
2022-08-15 00:35:10 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Title = styled.div`
+  display: flex;
+`;
+
+const parseLinks = (response) => {
+  const pages = response.data.query.pages;
+  let links = [];
+  for (const pageCode in pages) {
+    const page = pages[pageCode];
+    links = [...links, ...page.links.map(link => link.title.toLowerCase())];
+  }
+  return links;
+}
 
 function App() {
+  const [start, setStart] = useState("Paul Dirac");
+  const [end, setEnd] = useState("Nuclear Physics");
+  const [currentPlace, setCurrentPlace] = useState(start);
+  const [input, setInput] = useState("");
+  const [guess, setGuess] = useState("");
+  const [guesses, setGuesses] = useState([]);
+  const [gameOver, setGameOver] = useState(false);
+
+  useEffect(() => {
+    axios.get(`https://en.wikipedia.org/w/api.php?action=query&format=json&origin=*&prop=links&meta=&titles=${currentPlace}&pllimit=500`).then(
+    (response) => {
+      const links = parseLinks(response);
+      console.log(links)
+      if (links.indexOf(guess) !== -1) {
+        setCurrentPlace(guess);
+        setGuesses(guesses => [...guesses, guess]);
+        axios.get(`https://en.wikipedia.org/w/api.php?action=query&format=json&origin=*&prop=links&meta=&titles=${guess}&pllimit=500`).then(
+          (res) => {
+            const linksFromGuess = parseLinks(res);
+            console.log(linksFromGuess)
+            if (linksFromGuess.indexOf(end.toLowerCase()) !== -1) {
+              // GAME WON
+              setGameOver(true);
+              console.log("CONGRATS")
+            }
+          }
+        );
+      }
+
+    });
+  }, [guess]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Title>LINKER</Title>
+      <p>{start} -> {end}</p>
+      <input onChange={e => setInput(e.target.value)}/>
+      <button onClick={() => setGuess(input.toLowerCase())}>Guess</button>
+      {guesses?.map((guess, index) => (
+        <div key={index}>{guess}</div>
+      ))}
+    </Container>
   );
 }
 
